6c8dcd85afcc07fb13105bedf3833aa6deac9bef
nguyendang111999 <nguyendang111999@gmail.com>
2026-07-21 20:00:48 +0700

    ver 6

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -7,7 +7,7 @@ export const contact = {
   email: "haidang.gamedev@gmail.com", // e.g."hai@haidang.space"
   facebook: "https://www.facebook.com/the.kingdom.752/", // e.g. "https://www.facebook.com/haidang"
   linkedin: "https://www.linkedin.com/in/dang-nguyen-94279a268", // e.g. "https://www.linkedin.com/in/haidang"
-  github: "", // e.g. "https://github.com/haidang"
+  github: "https://github.com/nguyendang111999", // e.g. "https://github.com/haidang"
   itchio: "", // e.g. "https://haidang.itch.io"
 } as const;
 
